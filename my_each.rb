f78dbc0135d1de980bcbcb232057221b3cab4d5f
Mariam Haider <mfh.haider@gmail.com>
2020-04-01 02:49:03 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,21 @@
 def my_each(array)
   i = 0
   while i < array.length
+<<<<<<< HEAD
    yield array[i]
     i += 1
   end
 array
 end 
 
+=======
+   yield(array[i])
+    i += 1
+  end
+  array
+end 
+
+
+
+
+>>>>>>> 68550ca591d40979a0074c573cdc16e000661d6b
